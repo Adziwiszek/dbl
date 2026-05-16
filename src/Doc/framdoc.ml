213e2351e006b8d42076dc2336e1a9c8cda658c3
Adam Dziwiszek <adziwiszek1@gmail.com>
2026-05-16 16:23:40 +0200

    cleaning of framdoc.ls

--- src/Doc/framdoc.ml
+++ src/Doc/framdoc.ml
@@ -94,53 +94,6 @@ let render_html template bindings =
     template
     bindings
 
-(* IR of a single entry ==================================================== *)
-
-(** # Definition entries in a module *)
-
-let js_escape s =
-  s |> String.split_on_char '\\' |> String.concat "\\\\"
-    |> String.split_on_char '"'  |> String.concat "\\\""
-    |> String.split_on_char '\n' |> String.concat "\\n"
-
-let html_escape s =
-  s |> String.split_on_char '<' |> String.concat "&lt;"
-    |> String.split_on_char '>' |> String.concat "&gt;"
-    |> String.split_on_char '"' |> String.concat "&quot;"
-
-type entry = {
-  e_mod  : string;
-  e_name : string;
-  e_kind : string;   (* "val" | "let rec" | "type" *)
-  e_line : int;
-  e_sig  : string;   (* typ, na razie "" *)
-  e_doc  : string;
-}
-
-let all_entries : entry list ref = ref []
-
-let add_entry mod_name name line doc : unit =
-  let e = 
-    { e_mod = mod_name
-    ; e_name = name
-    ; e_kind = "val"
-    ; e_line = line
-    ; e_sig = "*"
-    ; e_doc = doc
-    }
-  in
-  all_entries := e :: !all_entries
-
-let entry_to_json e =
-  Printf.sprintf
-    {|{mod:"%s",name:"%s",kind:"%s",line:%d,sig:"%s",doc:"%s"}|}
-    (js_escape e.e_mod)
-    (js_escape e.e_name)
-    (js_escape e.e_kind)
-    e.e_line
-    (js_escape e.e_sig)
-    (js_escape (html_escape e.e_doc))
-
 (* IR of modules =========================================================== *)
 
 (** Single module *)

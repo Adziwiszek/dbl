e3be40ed8cba09aee16238aaaf81d9f201d61dbd
Adam Dziwiszek <adziwiszek1@gmail.com>
2026-08-05 23:49:11 +0200

    Improved tr_fn in CPS -> SExpr translation

--- src/Lang/CPSPriv/SExprPrinter.ml
+++ src/Lang/CPSPriv/SExprPrinter.ml
@@ -24,32 +24,32 @@ let rec tr_expr (e : cexp) : SExpr.t =
 
 	| App _ -> tr_app e
 
-  | Fix _ -> List (Sym "fn" :: tr_fn e)
+  | Fix _ -> tr_fn e
 
 	| Switch _ -> failwith "Switch sexpr not implemented" 
 	| Primop _ -> failwith "Primop sexpr not implemented" 
   | Halt _ -> failwith "Halt sexpr not implemented" 
 
-and tr_fn (e : cexp) : SExpr.t list =
+and tr_fn (e : cexp) : SExpr.t =
+  let aux_tr_fn x : SExpr.t = 
+      let (f, args, body) = x in 
+      let tr_args = List.map tr_var args in
+      List (Sym "fn" :: tr_var f :: tr_args @ [tr_expr body])
+  in
   match e with
   (* TODO: translate functions *)
   | Fix(fns, c) -> 
-    List.concat_map 
-    (fun (f, args, body) -> 
-      let tr_args = List.map tr_var args in
-      [tr_var f] @ tr_args @ [tr_expr body]
-    ) fns  
-    @ [tr_expr c]
-  | _ -> [ tr_expr e ]
+      List ( Sym "fix" :: List.map aux_tr_fn fns @ [tr_expr c] )
+  | _ -> failwith "tr_fn: Fix expected"
 
 and tr_app e =
   match e with
-  | App(v, args) -> List (tr_value v :: List.map tr_value args)
+  | App(v, args) -> List ( Sym "app" :: tr_value v :: List.map tr_value args)
   | _ -> failwith "tr_app: App expected"
 
 and tr_ctor e =
   match e with
-  | Ctor(n, vs) -> List ( Num n :: List.map tr_value vs )
+  | Ctor(n, vs) -> List ( Sym "ctor" :: Num n :: List.map tr_value vs )
   | _ -> tr_expr e
 
 let tr_program = tr_expr
